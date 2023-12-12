635077414b56d394aa5a7698b8723d454b32518a
Victor Mureithi <victormureithig@gmail.com>
2023-12-12 20:05:17 +0300

    First commit

---
+++ src/ui/input.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-grey-0);
+  padding: 0.8rem 1.2em;
+  box-shadow: var(--shadow-sm);
+`;
+export default input;
