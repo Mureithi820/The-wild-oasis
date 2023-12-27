37d0c8bd239f310e0a24c64eb4e45e3a0f186b05
Victor Mureithi <victormureithig@gmail.com>
2023-12-27 14:49:03 +0300

    Added user logout

--- src/ui/Form.jsx
+++ src/ui/Form.jsx
@@ -1,27 +1,74 @@
-import styled, { css } from "styled-components";
+// import styled, { css } from "styled-components";
+
+// const Form = styled.form`
+//   ${(props) =>
+//     props.type !== "regular" &&
+//     css`
+//       padding: 2.4rem 4rem;
+
+//       /* Box */
+//       background-color: #fff;
+//       border: 1px solid #f3f4f6;
+//       border-radius: 7px;
+//     `}
+
+//   ${(props) =>
+//     props.type === "modal" &&
+//     css`
+//       width: 80rem;
+//     `}
+
+//   overflow: hidden;
+//   font-size: 1.4rem;
+// `;
+// Form.defaultProps = {
+//   type: "regular",
+// };
+// export default Form;
+import styled from "styled-components";
 
 const Form = styled.form`
-  ${(props) =>
-    props.type !== "regular" &&
-    css`
-      padding: 2.4rem 4rem;
+  /* Basic Styles */
+  padding: ${(props) => (props.type === "modal" ? "2.4rem 4rem" : "2.4rem")};
+  background-color: #fff;
+  border: 1px solid #f3f4f6;
+  border-radius: 7px;
+  width: ${(props) =>
+    props.type === "modal" ? "80rem" : "fit-content"}; /* Adjusted width */
+  margin: 0 auto; /* Center the form on the page */
 
-      /* Box */
-      background-color: #fff;
-      border: 1px solid #f3f4f6;
-      border-radius: 7px;
-    `}
-
-  ${(props) =>
-    props.type === "modal" &&
-    css`
-      width: 80rem;
-    `}
-    
-  overflow: hidden;
   font-size: 1.4rem;
 `;
+
 Form.defaultProps = {
   type: "regular",
 };
+
 export default Form;
+
+// import styled from "styled-components";
+
+// const Form = styled.form`
+//   /* Basic Styles */
+//   padding: 2.4rem 4rem;
+//   background-color: #fff;
+//   border: 1px solid #f3f4f6;
+//   border-radius: 7px;
+
+//   /* Additional Styles for Modal */
+//   ${(props) =>
+//     props.type === "modal" &&
+//     `
+//     width: 80rem;
+//     padding: 2.4rem 4rem;
+//   `}
+
+//   overflow: hidden;
+//   font-size: 1.4rem;
+// `;
+
+// Form.defaultProps = {
+//   type: "regular",
+// };
+
+// export default Form;
--- src/ui/input.jsx
+++ src/ui/input.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 
-const input = styled.input`
+const Input = styled.input`
   border: 1px solid #d1d5db;
   border-radius: 5px;
   background-color: #fff;
   padding: 0.8rem 1.2em;
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.04);
 `;
-export default input;
+export default Input;
